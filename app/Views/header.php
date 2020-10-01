bf5ff5017fe0aa1e9a0904367f27c58b0a734252
leidy28ort <leidy28ortega@gmail.com>
2020-10-01 15:33:41 -0500

    cuarto commit

--- app/Views/header.php
+++ app/Views/header.php
@@ -77,15 +77,29 @@
 
     <!--Inicio-->
     
-
         <div class="home" id="inicio">
             <div class="home_background_container prlx_parent">
-                <div class="home_background prlx"
-                    style="background-image:url(<?php echo base_url();?>/public/img/Letrero.jpeg); transform: translate(0%, -15.6566%) translate3d(0px, 0px, 0px);}">
+                <div class="home_background prlx" style="background-image:url(<?php echo base_url();?>/public/img/Letrero.jpeg); transform: translate(0%, -15%) translate3d(0px, 0px, 0px);}">
+                </div>
+            </div>  
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #f5b003;"> 
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav nav-fill">
+                    <a class="nav-link active" href="#">Inicio <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Ofertas Departamentales</a>
+                    <a class="nav-link" href="#">Ofertas Nacionales</a>
+                    <a class="nav-link" href="#">Cotización</a>
+                    <a class="nav-link" href="#">Contacto</a>
+                    </div>
                 </div>
             </div>
-            
-        </div>
+        </nav>
 
 </body>    
 </html>
